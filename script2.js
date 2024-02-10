e63db4a87ce4d0782c003cd4a844d3d794c54cf6
ajoshi253 <joshiarina@gmail.com>
2024-02-09 19:10:56 -0600

    added

--- script2.js
+++ script2.js
@@ -1,2 +1,7 @@
 console.log("Hello script 1")
 console.log("Checking git diff")
+console.log("I am making some changes in script2")
+console.log("I am further changing script2.js")
+console.log("learning")
+
+
