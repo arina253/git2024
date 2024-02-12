ff56f5afe1561510c606d4dd635aace224efd81f
ajoshi253 <joshiarina@gmail.com>
2024-02-11 23:00:31 -0600

    added in local branch

--- script2.js
+++ script2.js
@@ -3,5 +3,7 @@ console.log("Checking git diff")
 console.log("I am making some changes in script2")
 console.log("I am further changing script2.js")
 console.log("learning")
+console.log("I am making some changes in local branch")
+
 
 
